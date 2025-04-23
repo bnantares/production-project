e62a9b02c6964adeca6e5e13a3a23eef160458a1
Anton Petrov <ant8151@yandex.ru>
2025-04-23 11:58:49 +0300

    FSD refactor

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import './styles/index.scss';
+import { Suspense } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
+
+const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+        <Link to={'/'}>Главная</Link>
+        <Link to={'/about'}>О сайте</Link>
+        <AppRouter />
+    </div>
+  )
+}
+
+export default App;
