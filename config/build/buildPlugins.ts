6e8fbc7ebecfd4df309ce9804e19bcf5065f5e9b
bnantares <ant8151@yandex.ru>
2025-05-07 13:52:57 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,24 +7,29 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
-           template: paths.html
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css[name].[contenthash:8].css',
-            chunkFilename: 'css[name].[contenthash:8].css'
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev)
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new ReactRefreshWebpackPlugin({
-            overlay: false,
-        }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        }),
+            template: paths.html
+         }),
+         new webpack.ProgressPlugin(),
+         new MiniCssExtractPlugin({
+             filename: 'css[name].[contenthash:8].css',
+             chunkFilename: 'css[name].[contenthash:8].css'
+         }),
+         new webpack.DefinePlugin({
+             __IS_DEV__: JSON.stringify(isDev)
+         }),
+         new ReactRefreshWebpackPlugin({
+             overlay: false,
+         }),
     ]
+    
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }));
+    }
+
+    return plugins;
 }
